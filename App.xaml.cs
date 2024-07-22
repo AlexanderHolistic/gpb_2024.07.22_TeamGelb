64130b25a8744727030ecfd9db28162de0ead6ea
Alexander Holike <alexander.holike@gmail.com>
2024-07-22 15:32:22 +0200

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Template
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            try
+            {
+                var myWindow = new MainWindow { ViewModel = new ViewModels.MainWindowViewModel() };
+                myWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                string message = string.Format("Exception: {0} \n\nStack: {1}", ex.Message, ex.StackTrace);
+            }
+        }
+
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+    }
+}
